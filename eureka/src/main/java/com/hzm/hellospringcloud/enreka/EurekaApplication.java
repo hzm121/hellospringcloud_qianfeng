b42952699cfcfb38638b0dc6c5a7406b03ea7d01
hzm121 <2696333036@qq.com>
2020-02-05 17:28:20 +0800

    完成eureka服务的搭建

---
+++ eureka/src/main/java/com/hzm/hellospringcloud/enreka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.hzm.hellospringcloud.enreka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : hzm
+ * @date : 2020-02-05 12:26
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
