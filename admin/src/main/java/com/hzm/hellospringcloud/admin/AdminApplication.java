021113f015597fa142aca33a556acaba53231ec7
hzm121 <2696333036@qq.com>
2020-02-09 18:30:22 +0800

    实现了用springboot对服务状态的监控

--- admin/src/main/java/com/hzm/hellospringcloud/admin/AdminApplication.java
+++ admin/src/main/java/com/hzm/hellospringcloud/admin/AdminApplication.java
@@ -1,12 +1,19 @@
 package com.hzm.hellospringcloud.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * @author : hzm
  * @date : 2020-02-09 00:02
  */
-
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
 public class AdminApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(AdminApplication.class,args);
     }
 }
